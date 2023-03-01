8ac4f76f0637a545bfea47b1fb15ca82f7e41676
Eve Cho <e.young.echo@gmail.com>
2023-03-01 10:39:21 -0500

    feature: tabs on navigation switch components onClick

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,28 +5,34 @@ import { MuteTag } from "./MuteTag";
 import { HowTo } from "./HowTo";
 
 export const Nav = () => {
+    const [tab, setTab] = useState(<HowTo/>);
 
+    const handleOnClick = (navTab) => () => {
+        const tabType = navTab;
+        console.dir(tabType);
+        setTab(tabType);
+    }
 
     return (
         <div>
             <div className={styles.center}>
-                <div className={styles.card}>
+                <div className={styles.card} onClick={handleOnClick('muteTag')}>
                     <p className={styles.description}>
                     Mute a Tag
                     </p>
                 </div>
-                <div className={styles.card}>
+                <div className={styles.card} onClick={handleOnClick('muteFic')}>
                     <p className={styles.description}>
                     Mute a Fic
                     </p>
                 </div>
-                <div className={styles.card}>
+                <div className={styles.card} onClick={handleOnClick('howTo')}>
                     <p className={styles.description}>
                     How to Use
                     </p>
                 </div>
             </div>
-            {true ? <HowTo/> : <MuteFic/>}
+            {tab}
         </div>
     )
 }
