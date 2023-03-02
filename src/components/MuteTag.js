84d91965183ebd2e5b3d7512f0f110e061ff39ae
Eve Cho <e.young.echo@gmail.com>
2023-03-01 21:10:23 -0500

    feature: added function and button to expand on array of links to add more than one

--- src/components/MuteTag.js
+++ src/components/MuteTag.js
@@ -11,12 +11,20 @@ export const MuteTag = () => {
     setLinks(newLinks);
   }
 
+  const addLine = e => {
+    e.preventDefault();
+    const oldLinks = [...links];
+    oldLinks.push('');
+    setLinks(oldLinks);
+  }
+
   return (
       <div className={styles.center}>
         <div>
           <h4>paste link(s) for the tag(s) you&apos;d like muted</h4>
           <form>
-            {links.map((link, ind) => <input key={ind} id={ind} type='text' value={link} onChange={handleChange}/>)}
+            {links.map((link, ind) => <input className={styles.addLink} key={ind} id={ind} type='text' value={link} onChange={handleChange}/>)}
+            <button onClick={addLine}>add another link +</button>
           </form>
         </div>
       </div>
