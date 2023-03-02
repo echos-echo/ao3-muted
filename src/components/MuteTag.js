f215ca79a232aa81f280b5843628c141bf150df6
Eve Cho <e.young.echo@gmail.com>
2023-03-01 21:23:09 -0500

    feature: submit button generates CSS for muting tags. deploying to test if clipboard API works for auto copying to clipboard

--- src/components/MuteTag.js
+++ src/components/MuteTag.js
@@ -18,15 +18,54 @@ export const MuteTag = () => {
     setLinks(oldLinks);
   }
 
+  /**
+   * @name getTagSelector
+   * gets the href of a tag given a link
+   * 
+   * @param link      string: the link for the page of the tag
+   * @returns         string: the formatted CSS to mute the tag
+   */
+  const getTagSelector = link => {
+    let tag = link.slice(link.indexOf('/tags'));
+    let cssFormattedTag = `.work.blurb.group:has(a[href^="${tag}"])`;
+    return cssFormattedTag;
+  }
+
+  const generateListBlockedTags = links => {
+    return links.map(link => getTagSelector(link)).join(', ') + ` { display: none !important; }`;
+  }
+
+  const handleSubmitForm = e => {
+    e.preventDefault();
+    setTagsCSS(generateListBlockedTags(links));
+  }
+
+  useEffect(() => {
+    let text = document.getElementById('mutedTagsCSS').innerHTML;
+    const copyContent = async () => {
+      try {
+        await navigator.clipboard.writeText(text);
+        alert('Content copied to clipboard');
+      } catch (err) {
+        console.error('Failed to copy: ', err);
+      }
+    }
+    copyContent();
+  }, [tagsCSS]);
+
   return (
       <div className={styles.center}>
         <div>
           <h4>paste link(s) for the tag(s) you&apos;d like muted</h4>
-          <form>
+          <form onSubmit={handleSubmitForm}>
             {links.map((link, ind) => <input className={styles.addLink} key={ind} id={ind} type='text' value={link} onChange={handleChange}/>)}
             <button onClick={addLine}>add another link +</button>
             <input type="submit"/>
           </form>
+        </div>
+        <div>
+          <h4>here is your CSS:</h4>
+          <p id="mutedTagsCSS">{tagsCSS}</p>
         </div>
       </div>
   )
